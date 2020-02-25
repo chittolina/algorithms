ae370a724aa74370804af818e40c9f03ae64683c
Gabriel Chittolina <gabrielchittolina1@gmail.com>
2020-02-25 16:59:32 -0300

    Added more tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -2,7 +2,7 @@ import {insert, remove} from '../bst'
 
 describe('Algorithms', () => {
   describe('BST', () => {
-    it('insert', () => {
+    it('insert 1', () => {
       const tree = {
         value: 10,
         left: {
@@ -55,7 +55,60 @@ describe('Algorithms', () => {
       expect(insert(tree, valueToInsert)).toEqual(expectedTree)
     })
    
-    it('remove', () => {
+    it('insert 2', () => {
+      const tree = {
+        value: 10,
+        left: {
+          value: 3,
+          left: {
+            value: 2,
+            left: null,
+            right: null,
+          },
+          right: {
+            value: 4,
+            left: null,
+            right: null,
+          }
+        },
+        right: {
+          value: 7,
+          left: null,
+          right: null
+        }
+      }
+  
+      const valueToInsert = 1
+      const expectedTree = {
+        value: 10,
+        left: {
+          value: 3,
+          left: {
+            value: 2,
+            left: {
+              value: 1,
+              left: null,
+              right: null
+            },
+            right: null,
+          },
+          right: {
+            value: 4,
+            left: null,
+            right: null,
+          }
+        },
+        right: {
+          value: 7,
+          left: null,
+          right: null
+        }
+      }
+  
+      expect(insert(tree, valueToInsert)).toEqual(expectedTree)
+    })
+  
+    it('remove 1', () => {
       const tree = {
         value: 10,
         left: {
@@ -97,6 +150,51 @@ describe('Algorithms', () => {
         }
       }
       
+      expect(remove(tree, valueToRemove)).toEqual(expectedTree)
+    })
+
+    it('remove 2', () => {
+      const tree = {
+        value: 10,
+        left: {
+          value: 3,
+          left: {
+            value: 2,
+            left: null,
+            right: null,
+          },
+          right: {
+            value: 4,
+            left: null,
+            right: null,
+          }
+        },
+        right: {
+          value: 11,
+          left: null,
+          right: null
+        }
+      }
+  
+      const valueToRemove = 2
+      const expectedTree = {
+        value: 10,
+        left: {
+          value: 3,
+          left: null,
+          right: {
+            value: 4,
+            left: null,
+            right: null
+          }
+        },
+        right: {
+          value: 11,
+          left: null,
+          right: null
+        }
+      }
+
       expect(remove(tree, valueToRemove)).toEqual(expectedTree)
     })
   })
