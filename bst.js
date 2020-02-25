7f9010bffd20312e41033d82f0e25bf0c2368e78
Gabriel Chittolina <gabrielchittolina1@gmail.com>
2020-02-25 16:57:28 -0300

    Fixed remove algorithm

--- bst.js
+++ bst.js
@@ -30,20 +30,26 @@ function insert(tree, value) {
 
 function remove(tree, value) {
   if (value < tree.value) {
-    tree.left = remove(tree.left, value)
+    if (tree.left) {
+      tree.left = remove(tree.left, value)
+    }
     return tree
   }
 
   if (value > tree.value) {
-    tree.right = remove(tree.right, value)
+    if (tree.right) {
+      tree.right = remove(tree.right, value)
+    }
     return tree
   }
   
   if (tree.value) {
-    // The new "parent" is the right node
+    // The new "parent" is the right node  
     if (tree.right) {
       tree.value = tree.right.value
       tree.right = tree.right.right
+    } else {
+      return null
     }
   }
 
